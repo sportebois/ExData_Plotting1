c54d60dc8af2f985e9209107e46abe442866c165
Sébastien Portebois <sportebois@gmail.com>
2015-02-06 23:23:22 -0500

    Adjust legend to match the templates (the small lines are now wider)

--- plot3.R
+++ plot3.R
@@ -30,6 +30,6 @@ points(dat$DateTime, dat$Sub_metering_2, type="l", col="red")
 points(dat$DateTime, dat$Sub_metering_3, type="l", col="blue")
 
 # Add legend
-legend("topright", pch="_", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty=1, seg.len=2, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 dev.off() # Clean the device
