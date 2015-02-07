3afce81620c1af1cbf7f1610e7440da85dd0bc97
Sébastien Portebois <sportebois@gmail.com>
2015-02-06 23:07:12 -0500

    Optim: check if the raw file has already been unzipped (git-ignored, because Github do not accept such a big file)

--- dataProvider.R
+++ dataProvider.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(tidyr)
 
 getUsableData <- function () {
-  # Load the raw dataset, filter only the timeframe we're interested in, 
+  # Load the raw dataset, filter only the timeframe we're interested in,
   # and add a computed timedate object
   #
   # Args:
@@ -11,27 +11,29 @@ getUsableData <- function () {
   # Returns:
   #   a data.frame with 10 variables:
   #  data.frame with  2880 obs. of  10 variables:
-  # Date                 : chr  
-  # Time                 : chr  
-  # Global_active_power  : num  
-  # Global_reactive_power: num  
-  # Voltage              : num  
-  # Global_intensity     : num  
-  # Sub_metering_1       : num  
-  # Sub_metering_2       : num  
-  # Sub_metering_3       : num  
-  # DateTime             : POSIXlt 
-  
+  # Date                 : chr
+  # Time                 : chr
+  # Global_active_power  : num
+  # Global_reactive_power: num
+  # Voltage              : num
+  # Global_intensity     : num
+  # Sub_metering_1       : num
+  # Sub_metering_2       : num
+  # Sub_metering_3       : num
+  # DateTime             : POSIXlt
+
   # Unzp the zip file, because the unzipped texte file is to huge for Github
-  unzip("data/exdata-data-household_power_consumption.zip", exdir="data")
-  
+  if (!file.exists("data/household_power_consumption.txt")) {
+      unzip("data/exdata-data-household_power_consumption.zip", exdir="data")
+  }
+
   tColNames <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
   tColClasses <- c("character", "character", "double", "double", "double", "double", "double", "double", "double")
   dat <- read.table("data/household_power_consumption.txt", header = TRUE, skip = 66636, nrows = 2880,sep = ";", na.strings = c("?", "", "NA"), colClasses = tColClasses)
   colnames(dat) <- tColNames
-  
+
   # Convert the dates
   dat$DateTime <- strptime(paste(dat$Date, dat$Time), "%d/%m/%Y %H:%M")
-  
+
   dat
 }
